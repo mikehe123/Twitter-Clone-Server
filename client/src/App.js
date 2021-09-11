e967c565400f1976efb50d1c880215d04d469299
mikehe123 <yhe48@u.rochester.edu>
2021-09-11 19:20:42 -0400

    implement Semantic UI & Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
-    <div>
-      <h1>Hi world!</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
