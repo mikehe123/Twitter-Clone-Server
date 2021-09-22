e9c569a028285a0b9b83d3abe28dfcbede380424
mikehe123 <yhe48@u.rochester.edu>
2021-09-21 21:36:06 -0400

    Prpared to deploy

--- client/src/util/MyPopup.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Popup } from "semantic-ui-react";
-
-export default function MyPopup({ content, children }) {
-  return <Popup inverted content={content} trigger={children} />;
-}
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config.js");
 
+const PORT = process.env.port || 5000;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -16,8 +17,11 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log("mongo db connected");
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.log(err);
   });
